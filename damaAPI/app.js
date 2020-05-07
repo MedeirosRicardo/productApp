f701e13e0c2a040da27c28350e0b40e64de9b1ee
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-05-07 15:49:33 -0400

    Created routes

--- damaAPI/app.js
+++ damaAPI/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const productRoutes = require('./routes/product');
 
 mongoose.connect(`mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@cluster0-apgkj.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 app.use(express.json());
+
+app.use('/products', productRoutes);
 
 module.exports = app;
