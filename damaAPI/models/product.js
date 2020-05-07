ccd91974b1670e9b5e6014030803172040a8d23b
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-05-07 15:27:09 -0400

    Model created

---
+++ damaAPI/models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    type: { type: String },
+    family: { type: String, required: true },
+    abv: { type: Number, required: true },
+    ibu: { type: Number, required: true },
+    malts: { type: [String], required: true },
+    hops: { type: [String], required: true },
+    medals: { type: [Sting] },
+    imageBottle: { type: String, required: true },
+    imageLogo: { type: String }
+});
+
+module.exports = productSchema;
