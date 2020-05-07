3460dbc4718201ea14e25313a521a0d11f4f90bf
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-05-07 15:44:21 -0400

    Created controller

---
+++ damaAPI/controllers/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Product = require('../models/product');
+
+const page = 1;
+const perPage = 5;
+
+exports.productGetAll = (req, res, next) => {
+    Product.find()
+        .skip(page * perPage)
+        .limit(perPage)
+        .exec()
+        .then(products => {
+            res.status(200).json(products);
+        })
+        .catch(err => res.status(500).json({
+            error: err
+        }));
+}
