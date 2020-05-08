ce2989cdd8d85a9a78e2466d5666d9e449259cb3
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-05-07 20:05:19 -0400

    Fixec start

--- damaAPI/app.js
+++ damaAPI/app.js
@@ -13,4 +13,19 @@ app.use(express.static('public'));
 
 app.use('/products', productRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
