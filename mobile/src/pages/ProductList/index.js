b8fba936721e0181dcb0a72e9fbed656e78df6fb
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-05-28 00:08:29 -0400

    fixed product layout

--- mobile/src/pages/ProductList/index.js
+++ mobile/src/pages/ProductList/index.js
@@ -1,12 +1,88 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text, FlatList, ScrollView, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
 export default function ProductList() {
+
+    const navigation = useNavigation();
+
+    function navigateBack() {
+        navigation.goBack();
+    }
+
     return (
-        <View>
-            <Text>Cervejas works!</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require('../../assets/logo.png')} />
+                <TouchableOpacity
+                    style={styles.leftArrow}
+                    onPress={navigateBack}
+                >
+                    <Feather name="arrow-left" size={28} color="#000000" />
+                </TouchableOpacity>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>CERVEJAS</Text>
+                </View>
+            </View>
+            <ScrollView style={styles.bottomContainer} >
+
+                <View style={styles.productContainer}>
+                    <View style={styles.productLeft}>
+                        <Text style={styles.productLeftSquare}>Square</Text>
+                        <Image
+                            style={styles.productLeftImage}
+                            source={require('../../assets/american-lager-355.png')}
+                        />
+                    </View>
+                    <View style={styles.productRight}>
+                        <Text style={styles.productTitle}>American Lager</Text>
+                        <Text style={styles.productText}>American Premium Lager</Text>
+                        <TouchableOpacity style={styles.productButton}>
+                            <Text style={styles.productButtonText}>SAIBA MAIS</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={styles.productContainer}>
+                    <View style={styles.productLeft}>
+                        <Text style={styles.productLeftSquare}>Square</Text>
+                        <Image
+                            style={styles.productLeftImage}
+                            source={require('../../assets/american-lager-355.png')}
+                        />
+                    </View>
+                    <View style={styles.productRight}>
+                        <Text style={styles.productTitle}>American Lager</Text>
+                        <Text style={styles.productText}>American Premium Lager</Text>
+                        <TouchableOpacity style={styles.productButton}>
+                            <Text style={styles.productButtonText}>SAIBA MAIS</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.productContainer}>
+                    <View style={styles.productLeft}>
+                        <Text style={styles.productLeftSquare}>Square</Text>
+                        <Image
+                            style={styles.productLeftImage}
+                            source={require('../../assets/american-lager-355.png')}
+                        />
+                    </View>
+                    <View style={styles.productRight}>
+                        <Text style={styles.productTitle}>American Lager</Text>
+                        <Text style={styles.productText}>American Premium Lager</Text>
+                        <TouchableOpacity style={styles.productButton}>
+                            <Text style={styles.productButtonText}>SAIBA MAIS</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+
+            </ScrollView>
+
         </View>
+
     );
 }
---
+++ mobile/src/pages/ProductList/styles.js
@@ -0,0 +1,93 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight + 20
+    },
+
+    header: {
+        height: 160,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+
+    bottomContainer: {
+        
+        marginTop: 20
+    },
+
+    titleContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 5,
+        paddingTop: 5
+    },
+
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold'
+    },
+
+    leftArrow: {
+        position: 'absolute',
+        top: 35,
+        left: 20
+    },
+
+    productContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 30
+    },
+
+    productLeft: {
+        width: '25%'
+    },
+
+    productLeftSquare: {
+        
+    },
+
+    productLeftImage: {
+        width: 254,
+        height: 238,
+        marginLeft: -57
+    },
+
+    productRight: {
+        width: '65%'
+    },
+
+    productTitle: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        color: '#000000',
+        lineHeight: 28,
+        paddingBottom: 30
+    },
+
+    productText: {
+        fontSize: 18,
+        color: '#000000',
+        paddingBottom: 70
+    },
+
+    productButton: {
+        width: 150,
+        height: 60,
+        borderStyle: 'solid',
+        borderColor: '#000000',
+        borderWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        left: 25
+    },
+
+    productButtonText: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#000000',
+    }
+});
