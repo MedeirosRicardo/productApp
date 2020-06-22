1f053b7564d777b94745532443e1a623fc6e3435
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-06-21 23:13:57 -0400

    added cors

--- damaAPI/app.js
+++ damaAPI/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const productRoutes = require('./routes/product');
 
 mongoose.connect(`mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@cluster0-apgkj.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
+{ useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
